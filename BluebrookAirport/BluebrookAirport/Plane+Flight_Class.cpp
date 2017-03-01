788168237562a1a7b07821a07462bbee81d8e054
SamLCrane <slcrane97@icloud.com>
2017-03-01 15:59:07 +0000

    simple booking system finished
    changes:
    -simple booking system finished:
    -ability to generate a new flight given the plane, departure time and
    arrival time (in future: add gate, destination, runway etc)
    -add booking given the flight, row and column number
    -print existing bookings given the flight
    -generateSeatingChart made into global function instead of part of the
    flight class
    -aisles attribute added to the plane class for use in visualising the
    seating charts
    
    to do:
    -implement departure and arrival time as time classes rather than
    strings
    -notification if a seat is booked or does not exist
    -seperate the classes out of the main files
    -begin to add user input

--- BluebrookAirport/BluebrookAirport/Plane+Flight_Class.cpp
+++ BluebrookAirport/BluebrookAirport/Plane+Flight_Class.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "stdafx.h"
+#include <cstdio>
+#include <fstream>
+#include <istream>
 #include <iostream>
 #include <string>
 #include <ctime>
@@ -27,50 +30,120 @@ plane::plane(string ID = "000000", int r = 0, int c = 0, int a = 0) {
 	rows = r;
 	columns = c;
 	aisles = a;
-	}
+}
 
-	class flight {
-		plane planeDetails;
-		string departureTime, arrivalTime;
-	public:
-		flight(plane, string, string);
-		int rows;
-		int columns;
-		int aisles;
-		void generateSeatingChart(int rows, int columns, int aisles) {
-			char ** seatingChart = new char *[columns];
+class flight {
+	plane planeDetails;
+	string departureTime, arrivalTime;
+public:
+	flight(string, plane, string, string);
+	string flightID;
+	int rows;
+	int columns;
+	int aisles;
+};
 
-			for (int i = 0; i < columns; i++) {
-				seatingChart[i] = new char[rows];
-
-				for (int j = 0; j < rows; j++) {
-					seatingChart[i][j] = 'x';
-				}
-			}
-
-			for (int i = 0; i <columns; i++) {
-				for (int j = 0; j < rows; j++) {
-					cout << seatingChart[i][j];
-				}
-				if (i == aisles - 1 || i == columns - aisles - 1) {
-					cout << "" << endl;
-				}
-				cout << "" << endl;
-			}
-		}
-	};
-
-	flight::flight(plane planeInfo, string departure, string arrival) {
-		planeDetails = planeInfo;
-		departureTime = departure;
-		arrivalTime = arrival;
-		rows = planeDetails.getRows();
-		columns = planeDetails.getColumns();
+flight::flight(string flightNumber, plane planeInfo, string departure, string arrival) {
+	flightID = flightNumber;
+	planeDetails = planeInfo;
+	departureTime = departure;
+	arrivalTime = arrival;
+	rows = planeDetails.getRows();
+	columns = planeDetails.getColumns();
 	aisles = planeDetails.getAisles();
 }
 
+//-----------------------------------------------------------------------------
+// Name: generateSeatingChart
+// Desc: Generate a new seating chart file
+//-----------------------------------------------------------------------------
+
+void generateSeatingChart(flight flight) {
+	ofstream flightFile;
+	flightFile.open(flight.flightID + ".txt");
+	for (int i = 0; i < flight.rows * flight.columns; i++) {
+		flightFile << "O" << endl;
+	}
+	flightFile.close();
+}
+
+//-----------------------------------------------------------------------------
+// Name: printSeatingChart
+// Desc: Print an existing seating chart
+//-----------------------------------------------------------------------------
+
+void printSeatingChart(flight flight) {
+	string tempString;
+	string seatingChart;
+
+	ifstream flightFile;
+	flightFile.open(flight.flightID + ".txt");
+
+	while (flightFile.is_open()) {
+		for (int i = 0; i < flight.columns; i++) {
+			for (int j = 0; j < flight.rows; j++) {
+				getline(flightFile, tempString);
+				seatingChart += tempString;
+			}
+			seatingChart += "\n";
+			if (i == flight.aisles - 1 || i == flight.columns - flight.aisles - 1) {
+				seatingChart += "\n";
+			}
+		}
+		flightFile.close();
+	}
+	cout << seatingChart << endl;
+}
+
+//-----------------------------------------------------------------------------
+// Name: addBooking
+// Desc: Adds a booked seat to the booking chart of a flight if it is available
+//-----------------------------------------------------------------------------
+
+void addBooking(flight flight, int row, int column) {
+	int seatID = ((((row - 1) * 5) + column - 1));
+	string seatValue;
+	ifstream inputFlightFile;
+	ofstream outputFlightFile;
+	inputFlightFile.open(flight.flightID + ".txt");
+	outputFlightFile.open(flight.flightID + "-temp.txt");
+
+	while (inputFlightFile.is_open()) {
+		for (int i = 0; i < flight.rows * flight.columns; i++) {
+			getline(inputFlightFile, seatValue);
+			if (i != seatID) {
+				if (seatValue == "O") {
+					outputFlightFile << "O\n";
+				}
+				else if (seatValue == "X") {
+					outputFlightFile << "X\n";
+				}
+			}
+			else {
+				if (seatValue == "O") {
+					outputFlightFile << "X\n";
+				}
+			}
+		}
+		inputFlightFile.close();
+		outputFlightFile.close();
+
+		ifstream  newFile(flight.flightID + "-temp.txt", ios::binary);
+		ofstream  oldFile(flight.flightID + ".txt", ios::binary);
+
+		oldFile << newFile.rdbuf();
+
+		oldFile.close();
+		newFile.close();
+		remove((flight.flightID + "-temp.txt").c_str());
+	}
+}
+
+
 /*int main() {
-plane testPlane("AAA001", 60, 10, 3);
-flight testFlight(testPlane, "12:00", "2:30");
-testFlight.generateSeatingChart(testFlight.rows, testFlight.columns, testFlight.aisles);
+	plane testPlane("AAA001", 5, 5, 2);
+	flight testFlight("FFF999", testPlane, "12:00", "2:30");
+	//generateSeatingChart(testFlight);
+	//addBooking(testFlight, 1, 1);
+	//printSeatingChart(testFlight);
 }*/
