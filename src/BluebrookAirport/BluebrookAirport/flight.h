72060bd3b739a2fe6fdca320feb12b92ce98a312
joezed <n0623403@my.ntu.ac.uk>
2017-04-24 17:51:59 +0100

    bug fix
    /\

--- src/BluebrookAirport/BluebrookAirport/flight.h
+++ src/BluebrookAirport/BluebrookAirport/flight.h
@@ -135,6 +135,32 @@ public:
 		}
 	}
 
+	int bermuda() {
+		int space, rows;
+
+		cout << "Enter number of rows: ";
+		cin >> rows;
+
+		for (int i = 1, k = 0; i <= rows; ++i, k = 0)
+		{
+			for (space = 1; space <= rows - i; ++space)
+			{
+				cout << "  ";
+			}
+
+			while (k != 2 * i - 1)
+			{
+				cout << "* ";
+				++k;
+			}
+			cout << endl;
+		}
+
+		bool hidden = true;
+
+		return 0;
+	}
+
 	void createXML() {
 
 		string flightNumber = flightID;
